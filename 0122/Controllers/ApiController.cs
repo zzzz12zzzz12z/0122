4319e1ea17b0b535047c7c1a81497d3481b287e2
shaomi <zzzz12zzzz12z@gmail.com>
2024-01-23 09:16:31 +0800

    加入專案檔案。

---
+++ 0122/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using _0122.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _0122.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _dbContext;
+
+        public ApiController(MyDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _dbContext.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
